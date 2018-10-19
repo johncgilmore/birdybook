456c0bff04f8771df70493f2c080da839c7f9017
John Gilmore <johncgilmore@gmail.com>
2018-10-19 12:32:24 -0700

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,135 +1,118 @@
-
 $(document).ready(function(){
-  setTimeout(function(){
-    //  animation of title
-    $('#headline').animate({opacity: 1}, 'slow'); 
-    $('.first').fadeIn(100);
-    $('.second').fadeIn("slow");
-    $('#isbn').focus();
-  }, 800);
-  $("#isbn").on('blur', function(event){
-    var isbn = $('#isbn').val();
-    $.get('/check?' + $.param({isbn: isbn}), function(response) {
-      
-      console.log("showing cover of " + response.delivery.query.title)
-      var cover = $(".book-cover")
-      cover.addClass('hidden')
-      cover.attr('src', response.delivery.query.imageLinks.thumbnail)
-      cover.slideDown()
-      $('#headline').text(response.delivery.headline)
-        
-
-    });
-  });  
-      /*$('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();*/
-  $("#zipcode").on('focus', function(){
-     var xTriggered = 0;
-    $( "#zipcode" ).keyup(function( event ) {
-      xTriggered++;
-      if (xTriggered > 3){
-        
-        var zipcode = $(this).val()
-        $.get('/books' + '?zipcode=' + zipcode, function(response){
-          $('main').html(response);
-});
-        //this.off('focus'); //doesn't work
-
-        
-        
-      } else {
-
-    }})
-  });
-  $("#demo").on('click', function(event){
+  
+  
+  $("#exampleIsbn").on('click', function(event){
     event.preventDefault();
-    var buttonText = $(this).text()
-    if (buttonText === "demo"){
-    $(this).text('back');
-    } else {
-      $(this).text('demo');
-    }
-
-    $('header frame ').toggleClass('hidden');
-    $(".video").toggleClass('hidden');
     
+    $(".isbn-input").val("9780134414232")
+    $(".isbn-input").focus();
+    $(".zip-input").focus();
   });
-  $("button").on('click', function(event){
-    console.log("clicked on a button item");
+  
+  $(".isbn-input").on('blur', function(event){
+    $("#submit").fadeOut("fast");
+    var isbn = $('#isbn').val();
+    if(isbn.length > 1){
+    $.get('/isbn?' + $.param({query: isbn}), function(data) {
+      var book = data.delivery;
+      //$('h3').remove();
+      console.log(book)
+      if(!book.cover){
+       console.log("no cover so we'll display the title")
+      } else {
+       console.log("showing cover of " + book.cover )
+      };
+        var cover = $("img");
+        cover.addClass('book-cover');
+       cover.attr("src", book.cover);
+       //$('h1').prepend(cover)
+      // $('form').append("<h3>" + book.volumeInfo.title + "</h3>");
+    });
+    }
   });
+  
+  $("#query").on('submit', function(event){
+    event.preventDefault();
+    
+  })
+  $( ".zip-input" ).keyup(function( event ) {
+    
+        var zipcode = $(this).val()
+        if (zipcode.length > 2){
+          $("#submit").fadeOut("fast");
+          $(this).removeClass(".zip-input");
+          
+          $.get('/schools' + '?zipcode=' + zipcode, function(response){
+            var schools = response.institutions.map(a)
+            });
+            var a = function(school){ 
+            //  $('.school-button').remove();
+              var button = "<button class='box black'><a class='institution' data-id='" +school.id + "' href='/ids/" + school.id + "/school/" + encodeURIComponent(school.name) + "'>" + school.name + "</a></button></a>"
+              console.log(button)
+              $('form').append(button);
+            
+           }
+            
+          }
+        });
+  
   $(".clip").on('click', function(){
     event.preventDefault();
     var repEmail = $('.repEmail').text()
     $('hidden').remove()
-    console.log("trying")
     $('button').remove();
-    var request = $('p').text()
+    var request = $('.request').text()
     
     var name = $('#firstName').val() + " " + $('#lastName').val();
     var profEmail = $('#email').val();
     var button = "<button class='greyout'> copy to clipboard</button> "
-    var message = "<textarea>" + request + "\n\n" + name + "\n\n" + profEmail + "</textarea>";
-    var mailTo = "<a href='mailto:" + repEmail + "'>" + repEmail + "</a>"
-    
-   // var emailLong = '<a href="mailto:' + repEmail + '"?cc=john@birdybook.com&bcc=johncgilmore@gmail.com, john@birdybook.com&subject=Desk%20Copy%20Request%For%20' + $('#firstName').val() + "%20" + $('#lastName').val() + '&body=' + message + '>Make Request</a>'
-
-    
-    var mail1 = "<p> This is in beta; for now you'll just have to copy this and paste it in an email!</p><p> Send it to "
-    console.log(message)
-    $('.pad').html(message + '<p> This is a beta. Do it the old fashioned way by simply copying and pasting this into an email to' + '\n' + '<h3>' + repEmail + '</h3>');
+    var message = request + "\n\n Thanks," + "\n " + name + " \n"
+    var textArea = "<textarea>" + message + "</textarea>";
+    var email = {
+      to: repEmail,
+      subject: 'Desk copy request for ' + name,
+      body: message,
+      from: profEmail
+    }
+    $('form').html(textArea + '<p> Modify the text above any way you like, then copy and paste it into an email to ' + email.to + '. ' + '\n' + 'Or click <a href="mailTo:' + email.to + '?From=' + email.from + '&Subject=' + email.subject + '&Body=' + email.body + '">send</a>')
     
 
   })
-  // for random emojis: TODO: select from a relavant list 
-  $('.switch').on('mouseenter', function(){
-    $.get('/research', function(emojis){
-      console.log(emojis)
-      $('.switch').find('#emojis').text(emojis);
-    });
-  });
+  
+  function sendMail() {
+    var link = "mailto:johncharlesgilmore@gmail.com"
+             + "?cc=myCCaddress@example.com"
+             + "&subject=" + escape("This is my subject")
+             + "&body=" + escape(document.getElementById('myText').value)
+    ;
+
+    window.location.href = link;
+}
+
+
   $('.resources').on('click', function(event){
     event.preventDefault();
-    var need = $(this).val();
-    $(this).fadeOut('fast')
+    var need = $(this).text();
+    $(this).addClass('greyed')
+    $(this).text(need + " √")
+    var name = $('#firstName').val() + " " + $('#lastName').val();
+    var profEmail = $('#email').val();
+    $('.request').append("<br><br>")
+    //$('.request').append(name)
+    $('#firstName').addClass('hidden')
+    $('#lastName').addClass('hidden')
     if($('.needs').text()) {
      $('.needs').append(" and ");
     }
      $('.needs').append(need);
+    // $('.hide').remove()
      $('.hidden').removeClass('hidden');
      $('#name').focus();
   });
-
-  /*
-  $('.preference').on('click', 'button', function(event){
-    event.preventDefault();
-    var preference = $(this).value();
-    $('.physical-digital').html(preference)
-    
-  })
   
-  $('#go').on('click', function(event){
-    event.preventDefault();    
-    if(!$('#isbn').val()){ 
-      var isbn = 9780393937510; 
-      alert("you didn't enter an isbn, but we'll use an example isbn of " + isbn + " so you can see how it works!");
-    }
-    else if(!$('#zipcode').val()){
-      var zipcode = 12467;
-      alert("you didn't enter your zipcode, but we'll show results for schools near zipcode "+ zipcode +" so you can see how it works!")
-    }
-    else if(!$('#zipcode').val()&& !$('#isbn').val()) {
-      var zipcode = 12467;
-      var isbn = 9780393937510;
-      alert("you didn't enter an isbn or zipcode, so we'll show demo data as an example!")
-    }
-    else {
-      var zipcode = $('#zipcode').val()
-      var isbn = $('#isbn').val() 
-    }
-    $('.wire').html('<span class="purple">Where</span> do you teach?');
-    $.get('/books?isbn=' + isbn + '&zipcode=' + zipcode, function (req, res){
-      console.log(res)
-      });
-  });*/
+  
 });
+
+
+
+
